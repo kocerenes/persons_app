3f3fc3c070f7a50b0034119d1f33b04a05897eb2
kocerenes <eneskocerr41@gmail.com>
2022-04-11 00:03:46 +0300

    Create design of person detail page.

---
+++ app/src/main/java/com/example/personsapp/PersonDetailPage.kt
@@ -0,0 +1,8 @@
+package com.example.personsapp
+
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+
---
+++ app/src/main/java/com/example/personsapp/PersonRegistryPage.kt
@@ -0,0 +1,67 @@
+package com.example.personsapp
+
+import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun PersonRegistrPage() {
+
+    val tfPersonName = remember {
+        mutableStateOf("")
+    }
+    val tfPersonTelNo = remember {
+        mutableStateOf("")
+    }
+    //geri tuşuna bastığımızda textFieldlardaki seçimi kaldırıcaz
+    val localFocusManager = LocalFocusManager.current
+
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text(text = "Kişi Kayıt") })
+        },
+        content = {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.SpaceEvenly,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                TextField(
+                    value = tfPersonName.value,
+                    onValueChange = { tfPersonName.value = it },
+                    label = { Text(text = "Kişi Adı") }
+                )
+                TextField(
+                    value = tfPersonTelNo.value,
+                    onValueChange = { tfPersonTelNo.value = it },
+                    label = { Text(text = "Kişi Tel No") }
+                )
+                Button(
+                    onClick = {
+                        val person_name = tfPersonName.value
+                        val person_tel = tfPersonTelNo.value
+                        Log.e("kayıt", "$person_name -- $person_tel")
+                        localFocusManager.clearFocus()
+                    },
+                    modifier = Modifier.size(250.dp, 50.dp)
+                ) {
+                    Text(text = "Kaydet")
+                }
+
+            }
+        }
+    )
+}
